83769bf9ddcda950867817d7d81ee6ea2fe476ea
SainTfactor <sta0003@auburn.edu>
2023-02-15 11:32:43 -0500

    Holy hell I finally got it!

--- 2022/16/solve.py
+++ 2022/16/solve.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from itertools import permutations
+from itertools import permutations, combinations
 import sys
 from data import data
 
@@ -51,57 +51,79 @@ def decide_target(current_loc, remaining_time):
     pot_value = (remaining_time - get_distance(current_loc, gv)) * data[gv]["rate"]
     print("AA to {}: {}".format(gv, pot_value))
 
-def process(good_valves, start_point, steps_left=30):
+def get_max(nodes, steps):
   global data
-  turns = permutations(good_valves)
+  ret_val = 0
+  for i in nodes:
+    ret_val += data[i]['rate']*steps
+  return ret_val
+
+def new_item(old_item, next_step, distance=-1):
+  global data
+  ret_val = {}
+
+  if distance == -1:
+    distance=get_distance(old_item['stop_point'], next_step)
+  if distance > old_item['steps_remaining']:
+    ret_val['path'] = old_item['path']
+    ret_val['steps_remaining'] = 0
+    ret_val['flow_rate'] = old_item['flow_rate']
+    ret_val['current_output'] = old_item['current_output'] + (old_item['flow_rate'] * old_item['steps_remaining'])
+    ret_val['stop_point'] = old_item['stop_point']
+    ret_val['done'] = True
+  else:
+    ret_val['path'] = old_item['path'] + [next_step]
+    ret_val['steps_remaining'] = old_item['steps_remaining'] - distance - 1
+    ret_val['flow_rate'] = old_item['flow_rate'] + data[next_step]['rate']
+    ret_val['current_output'] = old_item['current_output'] + (old_item['flow_rate'] * (distance + 1))
+    ret_val['stop_point'] = next_step
+    ret_val['done'] = False 
+  return ret_val
+
+def calculate_value(item):
+  global data, good_valves
+  ret_val = item['current_output']
+  ret_val += item['flow_rate'] * item['steps_remaining']
+  remaining_valves = [i for i in good_valves if not i in item['path']]
+  over_on_remaining = sum(data[i]['rate'] * item['steps_remaining'] for i in remaining_valves)
+  ret_val += int(over_on_remaining*0.5)
+  return ret_val
   
-  biggest=0
-  best_steps=0
-  route = None
-  for tgts in turns:
-    current_loc = start_point
-    steps = steps_left
-    output = 0
-    for step in tgts:
-      dst = get_distance(current_loc, step)
-      #print("{}, {}, {}".format(current_loc, step, dst))
-      steps -= dst + 1
-      if steps < 0:
-        break
-      output += steps*data[step]["rate"]
-      current_loc = step
-      #print("{}, {}, {}".format(30 - steps + 1, data[step]["rate"], output))
-    #print("{} - {}".format(tgts, output))
-    if biggest < output or (biggest == output and steps > best_steps):
-      biggest = output
-      best_steps = steps
-      route = tgts
-  return (biggest, route, best_steps if best_steps > 0 else 0)
 
-rate_range = sorted([data[i]["rate"] for i in data if data[i]["rate"] != 0])
+current_max=0
+good_path=None
+working_solves=[{ 'path' : [], 'done' : False, 'steps_remaining' : 30, 'flow_rate' : 0, 'current_output' : 0, 'stop_point' : start_point }]
+done = False
+count=0
+width=50
+while not done:
+  print(count)
+  count += 1
+  done=True
+  new_solves = []
+  for ws in working_solves:
+    if len(ws['path']) == len(good_valves):
+      new_step = new_item(ws, 'AA', 1000)
+      if new_step['current_output'] > current_max:
+        current_max = new_step['current_output']
+        good_path = new_step
+    else:
+      for gd in good_valves:
+        if not gd in ws['path']:
+          done=False
+          new_step = new_item(ws, gd)
+          #print(new_step)
+          if new_step['done']:
+            if new_step['current_output'] > current_max:
+              current_max = new_step['current_output']
+              good_path = new_step
+          else:
+            new_solves.append(new_step)
+        
+  working_solves = sorted(new_solves, key=calculate_value, reverse=True)[0:width]
+ 
+print(good_path) 
+print(current_max)
 
-#split_point = int(sys.argv[1])
-range_1 = rate_range[12:]
-range_2 = rate_range[8:12]
-range_3 = rate_range[4:8]
-range_4 = rate_range[0:4]
-print(range_1, range_2, range_3, range_4)
-
-print("-"*100)
-b1, r1, steps_left = process([i for i in good_valves if data[i]["rate"] in range_1], start_point)
-print(b1, r1, steps_left)
-print("-"*100)
-b2, r2, steps_left = process([i for i in good_valves if data[i]["rate"] in range_2], r1[-1], steps_left)
-print(b2, r2, steps_left)
-print("-"*100)
-b3, r3, steps_left = process([i for i in good_valves if data[i]["rate"] in range_3], r2[-1], steps_left)
-print(b3, r3, steps_left)
-print("-"*100)
-b4, r4, steps_left = process([i for i in good_valves if data[i]["rate"] in range_4], r3[-1], steps_left)
-print(b4, r4, steps_left)
-print("-"*100)
-
-print("="*100)
-print(b1+b2+b3+b4)
 # --------------------- Part 2 --------------------- #
 
