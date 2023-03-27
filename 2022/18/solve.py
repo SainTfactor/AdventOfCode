a991b2654f77748a32e6649183619318c66a22ce
SainTfactor <sta0003@auburn.edu>
2023-03-27 14:29:50 -0400

    Finished the thing.

--- 2022/18/solve.py
+++ 2022/18/solve.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
+
 from data import data
 
-data = [(2,2,2), (1,2,2), (3,2,2), (2,1,2), (2,3,2), (2,2,1), (2,2,3), (2,2,4), (2,2,6), (1,2,5), (3,2,5), (2,1,5), (2,3,5)]
+#data = [(2,2,2), (1,2,2), (3,2,2), (2,1,2), (2,3,2), (2,2,1), (2,2,3), (2,2,4), (2,2,6), (1,2,5), (3,2,5), (2,1,5), (2,3,5)]
 
 def check_open_sides(point,grid):
   x,y,z=point
@@ -39,7 +40,21 @@ print(total_open_sides)
 # --------------------- Part 2 --------------------- #
 
 def clear_neighbors(point,grid):
-  pass
+  x,y,z=point
+  ret_val = []
+  if (x+1) < len(grid) and not grid[x+1][y][z]:
+    ret_val.append((x+1,y,z))
+  if (x-1) >= 0 and not grid[x-1][y][z]:
+    ret_val.append((x-1,y,z))
+  if (y+1) < len(grid[x]) and not grid[x][y+1][z]:
+    ret_val.append((x,y+1,z))
+  if (y-1) >= 0 and not grid[x][y-1][z]:
+    ret_val.append((x,y-1,z))
+  if (z+1) < len(grid[x][y]) and not grid[x][y][z+1]:
+    ret_val.append((x,y,z+1))
+  if (z-1) >= 0 and not grid[x][y][z-1]:
+    ret_val.append((x,y,z-1))
+  return ret_val
 
 gas = [(0,0,0)]
 while len(gas) > 0:
@@ -50,3 +65,20 @@ while len(gas) > 0:
     new_gas += clear_neighbors(g,array_boi)
   gas = list(set(new_gas))
 
+
+data2 = []
+for x in range(len(array_boi)):
+  for y in range(len(array_boi[x])):
+    for z in range(len(array_boi[x][y])):
+      array_boi[x][y][z] = not array_boi[x][y][z]
+      if array_boi[x][y][z]:
+        data2.append((x,y,z))
+
+
+new_total_open_sides = 0
+for point in data2:
+  sides = check_open_sides(point, array_boi)
+  new_total_open_sides += sides
+
+print(new_total_open_sides)
+print("Final: {}".format(total_open_sides - new_total_open_sides))
