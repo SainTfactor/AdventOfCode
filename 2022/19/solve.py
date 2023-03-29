7619dee79f794aa4f5c87862682a8f8a047dd876
SainTfactor <sta0003@auburn.edu>
2023-03-29 16:44:37 -0400

    data imported

---
+++ 2022/19/solve.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+from data import blueprints
+
+blueprints = [
+  { 
+    "ore_robot" : { "ore" : 4 }, 
+    "clay_robot" : { "ore" : 2 }, 
+    "obsidian_robot" : { "ore" : 3, "clay" : 14 }, 
+    "geode_robot" :{ "ore" : 2, "obsidian" : 7 } 
+  }, 
+  { 
+    "ore_robot" : { "ore" : 2 }, 
+    "clay_robot" : { "ore" : 3 }, 
+    "obsidian_robot" : { "ore" : 3, "clay" : 8 }, 
+    "geode_robot" :{ "ore" : 3, "obsidian" : 12 } 
+  } 
+]
+
+print(data)
+
+# --------------------- Part 2 --------------------- #
+
+print(data)
