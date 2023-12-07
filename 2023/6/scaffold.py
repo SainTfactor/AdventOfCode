4634e7e73a7fdcdc16e874f1729b449375f2f703
SainTfactor <sta0003@auburn.edu>
2023-12-06 20:56:56 -0500

    minor changes

--- 2023/6/scaffold.py
+++ 2023/6/scaffold.py
@@ -3,7 +3,13 @@
 data = [[7,9], [15,40], [30,200]] # test data
 ata = [[56,334],[71,1135],[79,1350],[99,2430]] # real data
 
-print(data)
+def race_option_count(race_time, previous_best):
+    retval = -(race_time % 2)
+
+
+outsizes = []
+for datum in data:
+    pass
 
 # --------------------- Part 2 --------------------- #
 
