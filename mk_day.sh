b4e0d8f79ba737bfe8c6afd938e8be85a007014c
tyler <sta0003@auburn.edu>
2024-12-09 17:11:48 -0500

    Minor bugfix and added day 6 scaffold.

--- mk_day.sh
+++ mk_day.sh
@@ -3,6 +3,11 @@
 year=$1
 day=$2
 
+if [ "$day" == "" ]; then
+  echo "Usage: ./mk_day.sh <year> <day>"
+  exit
+fi  
+
 mkdir -p ./$year/$day
 if [ ! -f ./$year/$day/sol.py ]; then
   cp scaffold.py ./$year/$day/sol.py
