b2d5ba5740977a027f333834041cd5b18b677052
tyler <sta0003@auburn.edu>
2024-12-10 20:31:13 -0500

    started problem 9

---
+++ 2024/9/sol.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import argparse
+
+def parse_puzzle_input(real_data=False):
+  data_source = "real_data.txt" if real_data else "sample_data.txt"
+  with open(data_source, "r") as f:
+    data = [i.strip() for i in f.readlines()][0]
+
+  data = [int(i) for i in data]
+  return data
+
+
+
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser(prog="AoC Solver", description="This is a scaffold of an Advent of Code solver program.")
+  parser.add_argument("-r", "--real", action="store_true", help="Use real data instead of sample data.")
+  args = parser.parse_args()
+
+  data = parse_puzzle_input(args.real)
+
+  # --------------------- Part 1 --------------------- #
+
+
+
+
+  # --------------------- Part 2 --------------------- #
+
+
+
+
