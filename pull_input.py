2ebca8cc4886b817ced4b4f56e64e4564176f9b9
tyler <sta0003@auburn.edu>
2024-12-11 11:38:36 -0500

    Minor updates to help not have bad data pulls in future.

--- pull_input.py
+++ pull_input.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
+import os
 import requests
 import argparse
+from datetime import datetime
 
-def get_input(day):
+def get_input(day, year):
     session_id = ""
-    with open("session_id.dat", "r") as f:
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    filepath = os.path.join(script_dir, "session_id.dat")
+    with open(filepath, "r") as f:
         session_id=f.read().strip()
     s = requests.Session()
     s.cookies.set("session", session_id, domain="adventofcode.com")
-    data = s.get("https://adventofcode.com/2021/day/{}/input".format(day))
+    data = s.get("https://adventofcode.com/{}/day/{}/input".format(year,day))
     return data.text
 
 def array_of_strings(data):
@@ -22,9 +26,15 @@ def choose_display_type(data):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process Advent of Code input')
     parser.add_argument('-d', '--day', type=int, help='The day of the challenge you want your input for.')
+    parser.add_argument('-y', '--year', type=int, help='The day of the challenge you want your input for.')
     args = parser.parse_args()
 
-    data = get_input(args.day)
+    year = args.year
+    if year == None:
+      year = datetime.now().year
 
-    print("#!/usr/bin/env python3")
-    choose_display_type(data)
+    data = get_input(args.day, year)
+
+    #print("#!/usr/bin/env python3")
+    #choose_display_type(data)
+    print(data)
